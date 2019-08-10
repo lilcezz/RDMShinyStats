d6fe7963e029cad428dd87bd4c2aaa8ba6918dc0
chuckleslove <32628243+chuckleslove@users.noreply.github.com>
2019-08-09 19:52:00 -0500

    Add files via upload

---
+++ ShinyStats.js
@@ -0,0 +1,118 @@
+const Discord=require('discord.js');
+const bot=new Discord.Client();
+const ontime = require('ontime');
+const mysql = require('mysql');
+const config=require('./shinyConfig.json');
+const pokemonNames=require('./pokemon.json');
+
+
+/*
+CREATE TABLE `shiny_stats` (
+  `date` date NOT NULL,
+  `pokemon_id` smallint(6) unsigned DEFAULT NULL,
+  `count` int(11) unsigned DEFAULT '0',
+  `shiny_count` int(11) unsigned DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+*/
+
+const possibleShiny = [1,4,7,10,16,19,23,25,27,29,32,41,50,54,56,58,60,66,74,77,81,86,88,90,92,95,96,98,103,104,109,116,123,127,129,131,133,138,140,142,147,152,155,158,177,179,190,191,198,200,204,209,213,215,220,225,228,246,252,255,258,261,263,270,276,278,280,287,296,302,303,304,307,311,312,315,320,325,327,333,337,338,345,347,349,351,353,355,359,361,366,370,371,374,403,425,427,436];
+
+bot.login(config.token);
+
+ontime({
+	cycle:[config.runTime]
+}, async function(ot) {
+
+	await CalculateShinyStats(config.sqlConnection,config.outputChannel);
+
+	return ot.done();
+});
+
+async function CalculateShinyStats(database, channel)
+{
+	
+	let connection = mysql.createConnection(database);
+
+	connection.connect(async function(error) {
+		if(error)
+		{
+			console.log("Error connecting to SQL: "+error.stack);
+				connection.end(function(err) {
+					
+				});
+			return;
+		}
+
+		let pokemonStats = await GetShinyStats(connection);
+
+		let date = new Date();
+		let month = date.getMonth()+1;
+		
+		if(month == 13) {month = 1;}
+
+		let dateString = month+"-"+date.getDate()+"-"+date.getFullYear();
+
+		await bot.channels.get(channel).send("**Incoming shiny stats for "+dateString+"**");
+
+		for(var pokemon in pokemonStats)
+		{
+			await InsertShinyStats(connection,pokemonStats[pokemon]);
+
+			let pokemonName = pokemonNames[pokemonStats[pokemon].pokemon_id].name;
+
+			await bot.channels.get(channel).send("**"+pokemonName+"**  |  **"+pokemonStats[pokemon].shiny+"** shiny out of **"+pokemonStats[pokemon].total+"** total seen in the last 24 hours");			
+		}
+
+		connection.end();
+
+		return;
+	});
+	
+	
+}
+
+async function GetShinyStats(connection)
+{
+	return new Promise(function(resolve){
+	connection.query("SELECT pokemon_id,shiny FROM pokemon where shiny is not null AND updated > UNIX_TIMESTAMP(NOW() - INTERVAL 24 HOUR);", async function(error, results) {
+			
+		let pokemonStats = {};
+		
+		for(let i = 0; i < results.length; i++)
+		{
+			let currentPokemon = results[i];
+			if(currentPokemon.pokemon_id > 0 && possibleShiny.indexOf(currentPokemon.pokemon_id) >= 0)
+			{
+				if(!pokemonStats[currentPokemon.pokemon_id])
+				{
+					pokemonStats[currentPokemon.pokemon_id] = {shiny:0,total:0};
+				}
+
+				pokemonStats[currentPokemon.pokemon_id].pokemon_id = currentPokemon.pokemon_id;
+				pokemonStats[currentPokemon.pokemon_id].shiny += currentPokemon.shiny;
+				pokemonStats[currentPokemon.pokemon_id].total += 1;
+				
+			}
+		}
+		return resolve(pokemonStats);
+	});
+	});
+}
+
+async function InsertShinyStats(connection, pokemon)
+{
+	return new Promise(async function(resolve) {
+
+		
+
+		let sqlQuery = "INSERT INTO shiny_stats (date,pokemon_id,count,shiny_count) VALUES (CURDATE(),"+pokemon.pokemon_id+","+pokemon.total+","+pokemon.shiny+");";
+
+		
+		connection.query(sqlQuery, function(error,results) {
+
+			return resolve(true);
+		});
+
+		
+	});
+}
